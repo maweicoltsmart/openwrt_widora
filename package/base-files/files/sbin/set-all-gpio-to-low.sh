df224755e9f109293ab8414c124cc88c99e4bdaa
horse Ma <wei_technology@163.com>
2017-08-01 02:44:45 -0700

    add test gpio shell & add ppp aplication and shells

---
+++ package/base-files/files/sbin/set-all-gpio-to-low.sh
@@ -0,0 +1,108 @@
+cd /sys/class/gpio
+cd gpio37
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio17
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio41
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio42
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio11
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio6
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio40
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio39
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio14
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio15
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio16
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio18
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio19
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio22
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio23
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio24
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio25
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio26
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio27
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio28
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio29
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio0
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio1
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio2
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio3
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio4
+echo 0 > value
+
+cd /sys/class/gpio
+cd gpio5
+echo 0 > value
+
