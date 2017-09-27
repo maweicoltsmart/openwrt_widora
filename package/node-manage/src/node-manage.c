6a82b10d6ca60c8eaca325b58800ac77af1abc73
HorseMa <wei_technology@163.com>
2017-09-27 10:08:42 +0800

    add LoRaMAC&node-manage app

---
+++ package/node-manage/src/node-manage.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+#include <signal.h>
+#include "pthread.h"
+#include <sys/socket.h>
+#include <linux/wireless.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
+
+int main(int argc, char* argv[])
+{
+	
+	return 0;
+}
