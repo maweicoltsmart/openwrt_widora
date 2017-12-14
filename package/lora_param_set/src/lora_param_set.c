1d44d77bb2f89f8c67f84c4024f966cf4dcee096
horse Ma <wei_technology@163.com>
2017-12-14 05:27:46 -0800

    cfg.html use cgi is ok

--- package/lora_param_set/src/lora_param_set.c
+++ package/lora_param_set/src/lora_param_set.c
@@ -1,50 +1,21 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <json-c/json.h>
-#include <time.h>     //C语言的头文件
 
-int main(void) 
+int main(void)
 {
-    int i,n;
-    printf("Contenttype:text/plainnn");
-    n=0;
-    if(getenv("CONTENT-LENGTH")) 
-        n = atoi(getenv("CONTENT-LENGTH")); 
-    unsigned char* html = malloc(n);
-    gets(html);
-    struct json_object *json = (struct json_object *)(html);
-    struct json_object *node = NULL;
-    //cJOSN_GetObjectItem 根据key来查找json节点 若果有返回非空
-#if 0
-    if(1 == cJSON_HasObjectItem(json,"devices"))
-    {
-        printf("found devices node\n");
-    }
-    else
-    {
-        printf("not found devices node\n");
-    }
-#endif
-    node = json_object_object_get(json,"devices");
-#if 0
-    if(node->type == cJSON_Array)
-    {
-        printf("array size is %d\r\n",cJSON_GetArraySize(node));
-    }
-    else
-    {
-        printf("devices is not a node\r\n");
-        return 0;
-    }
-#endif
-    struct json_object *obj;
-    for(i = 0; i < json_object_array_length(node); i++) {
-      obj = json_object_array_get_idx(node, i);
-      printf("\t[%d]=%s\n", i, json_object_to_json_string(obj));
-    }
+    char *data;
+    long m,n;
 
-    json_object_put(json);//free  
-    free(html);
-} 
+    printf("%s\n\n","Content-Type:text/html;charset=utf-8");
+    printf("<TITLE>乘法结果</TITLE> ");
+    printf("<H3>乘法结果</H3> ");
+
+    data = getenv("QUERY_STRING"); //获得form传来的参数——两个乘数m和n
+    if(data == NULL)
+        printf("<P>错误！数据没有被输入或者数据传输有问题");
+    else if(sscanf(data,"m=%ld&n=%ld",&m,&n)!=2)
+        printf("<P>错误！输入数据非法。表单中输入的必须是数字。");
+    else
+        printf("<P>%ld和%ld的乘积是：%ld。",m,n,m*n);
+    return 0;
+}
